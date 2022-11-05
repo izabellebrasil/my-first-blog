5353c00c3e3bc5fbe0f5f24f5066daddca3c091a
Izabelle Brasil <izabellebrasil@hotmail.com>
2022-11-05 16:50:54 -0300

    my django girls app, first commit

---
+++ first.py
@@ -0,0 +1,18 @@
+def hi(izabelle):
+    print('hi ' +  izabelle + '!')
+hi (" girls")
+girls = ['vivian','izabelle']
+for name in girls:
+    hi(name)
+    print('Next girl')
+for i in range(1, 6):
+    print(i)
+    
+
+
+
+
+
+
+
+
